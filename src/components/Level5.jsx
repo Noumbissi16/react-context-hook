fd74d9bbd9b28148199164aea03bcbfc10381a84
noumbissi16 <noumbissistael@gmail.com>
2023-09-13 23:17:14 +0100

    learning react hook context

--- src/components/Level5.jsx
+++ src/components/Level5.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { ThemeModeContext } from "../contexts/ThemeModeContext";
+
 export function Level5(props) {
+  const { themeMode, setThemeMode } = useContext(ThemeModeContext);
+  const toggleThemeMode = () => {
+    setThemeMode(themeMode === "light" ? "dark" : "light");
+  };
   return (
     <>
       <div>I am level 5</div>
+      <button onClick={toggleThemeMode}> Toggle theme mode</button>
     </>
   );
 }
